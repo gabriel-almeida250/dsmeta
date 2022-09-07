03967a663162d36bf480415ed943e90b21e4ea21
Gabriel Almeida <gabrielalmeida2612@outlook.com>
2022-09-06 22:09:38 -0300

    First component

---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,14 @@
+import iconNotificar from '../../assets/img/notification-icon.svg';
+import './styles.css'
+
+function NotificationButton() {
+    return (
+        <>
+            <div className="dsmeta-red-btn">
+                <img src={iconNotificar} alt="Notificar" />
+            </div>
+        </>
+    );
+};
+
+export default NotificationButton;
